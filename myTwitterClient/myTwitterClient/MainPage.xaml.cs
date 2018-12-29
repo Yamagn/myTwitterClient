bee287dfb1e79e47027dda72123455500f434af6
Yamagn <syankenpon@gmail.com>
2018-12-30 07:55:14 +0900

    Initial Commit

---
+++ myTwitterClient/myTwitterClient/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace myTwitterClient
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void TweetButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+	}
+}
